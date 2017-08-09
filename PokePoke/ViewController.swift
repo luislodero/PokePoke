a9bd8f1c2a01f7c80e013f647f15f8ec9a768539
Luis Lopez <luislodero@gmail.com>
2017-08-08 19:35:08 -0500

    Got the map not fully working

--- PokePoke/ViewController.swift
+++ PokePoke/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            print("Ready for ya")
+            mapView.showsUserLocation = true
+
+        } else {     manager.requestWhenInUseAuthorization()
+
+            
+        }
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
